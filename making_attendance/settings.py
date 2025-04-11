0ea0105078517065eded8d0503e3f427c3736148
steam-making <robotmaking@naver.com>
2025-04-12 01:43:06 +0900

    add host

--- making_attendance/settings.py
+++ making_attendance/settings.py
@@ -42,7 +42,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '172.30.1.100']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '172.30.1.100','.onrender.com']
 
 
 # Application definition
