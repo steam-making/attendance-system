e6bfc2e577b052cf31ca57b4f598182f5983d43b
MeduTech <admin@medutech.kr>
2026-02-03 00:25:38 +0900

    출결리스트 UI

--- attendance/templatetags/extra_filters.py
+++ attendance/templatetags/extra_filters.py
@@ -4,3 +4,9 @@ register = template.Library()
 @register.filter
 def dict_get(d, key):
     return d.get(key)
+
+@register.filter
+def split(value, delimiter):
+    if value is None:
+        return []
+    return str(value).split(delimiter)
