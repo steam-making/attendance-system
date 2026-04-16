84021d85e37594dd9ce0c05e480e514ad05f9994
MeduTech <admin@medutech.kr>
2026-04-16 13:50:36 +0900

    fix: PaymentLog 임포트 누락 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, PaymentLog
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
